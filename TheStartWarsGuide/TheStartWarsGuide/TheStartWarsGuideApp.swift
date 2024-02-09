38022216b34eb29dc63ae0209c03c93d14e9694e
Gonzalo Giampietri <giampietri.gonzalo@gmail.com>
2024-02-09 10:08:18 -0300

    Initial project setup

---
+++ TheStartWarsGuide/TheStartWarsGuide/TheStartWarsGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStartWarsGuideApp.swift
+//  TheStartWarsGuide
+//
+//  Created by Gonzalo.Giampietri on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TheStartWarsGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
